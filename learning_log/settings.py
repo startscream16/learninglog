c821c0425019a99fde1c798f11af560e4f2225db
captain_gallup <startscream16@gmail.com>
2023-04-24 19:10:48 +0500

    test5

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'learning_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -141,4 +141,6 @@ django_heroku.settings(locals())
 if os.environ.get('DEBUG') == 'TRUE':
     DEBUG = True
 elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
+elif os.environ.get('DEBUG') == None:
     DEBUG = False
